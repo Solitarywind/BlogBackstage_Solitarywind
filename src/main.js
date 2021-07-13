ece767fdd238a291edbe79ca0a69b417dfaeb112
SolitaryWind <1304470481@qq.com>
2021-07-13 17:07:15 +0800

    第一次提交,npm 了element-ui,mavon-editor,nprogress

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import element from 'element-ui';
+import editor from 'mavon-editor';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
+Vue.use(element);
+Vue.use(editor);
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,44 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+import 'nprogress/nprogress.css';
+import { start, done, configure } from 'nprogress';
 
 Vue.use(VueRouter);
+
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
+configure({
+  showSpinner: false,
+});
+
+function delay(duration) {
+  return new Promise(((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  }));
+}
+
+function getPageComponent(pagefn) {
+  return async () => {
+    start();
+    if (process.env.NODE_ENV === 'development') {
+      await delay(1000);
+    }
+    const comp = await pagefn();
+    done();
+    return comp;
+  };
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: getPageComponent(() => import('../views/Home.vue')),
   },
   {
     path: '/about',
@@ -16,13 +46,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: getPageComponent(() => import(/* webpackChunkName: "about" */ '../views/About.vue')),
   },
 ];
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes,
 });
 
